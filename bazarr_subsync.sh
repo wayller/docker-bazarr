41709656f04816dffaada67dc55485da5c07364f
wayller <janpfo@live.no>
2020-04-16 09:33:57 +0200

    Update bazarr_subsync.sh

--- bazarr_subsync.sh
+++ bazarr_subsync.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/with-contenv bash
 
+echo "Installing Subsync, this will take 10-15 minutes."
+
 apt update
 apt install -y git swig python3-pip libsphinxbase-dev libpocketsphinx-dev libavutil-dev libswscale-dev libavformat-dev libavdevice-dev
 mkdir build
@@ -7,3 +9,7 @@ cd build
 git clone -b '0.15' https://github.com/sc0ty/subsync.git
 cd subsync
 pip3 install .
+rm -r build/
+apt purge -y python3-pip python3-setuptools
+
+echo "Finished installing Subsync!"
